fdc66219705f594f0b811af3e682d8bc0a063446
maxjonasf <jonasfohrbeck@gmail.com>
2022-04-12 00:15:57 +0200

    updated menu js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hello! Welcome to my portfolio site");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 641) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
