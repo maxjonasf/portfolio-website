1a100963bbe8ac3655a819d6d3432f51ca0b822d
maxjonasf <jonasfohrbeck@gmail.com>
2022-04-13 00:52:25 +0200

    Linted HTML, CSS and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hello! Welcome to my portfolio site");
+console.log('Hello! Welcome to my portfolio site')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle' && $(window).width() < 641) {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
